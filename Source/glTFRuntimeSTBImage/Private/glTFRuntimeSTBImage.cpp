573c20a8ee3bbf4d0ad18cf0bb793380c9344070
rdeioris <roberto.deioris@gmail.com>
2022-08-20 10:01:07 +0200

    improved chaining support

--- Source/glTFRuntimeSTBImage/Private/glTFRuntimeSTBImage.cpp
+++ Source/glTFRuntimeSTBImage/Private/glTFRuntimeSTBImage.cpp
@@ -15,6 +15,11 @@ void FglTFRuntimeSTBImageModule::StartupModule()
 
 	FglTFRuntimeParser::OnTexturePixels.AddLambda([](TSharedRef<FglTFRuntimeParser> Parser, TSharedRef<FJsonObject> JsonImageObject, TArray64<uint8>& CompressedPixels, int32& Width, int32& Height, TArray64<uint8>& UncompressedPixels)
 		{
+			// skip if already processed
+			if (UncompressedPixels.Num() > 0)
+			{
+				return;
+			}
 			int32 ChannelsInFile;
 			uint8* Pixels = stbi_load_from_memory(CompressedPixels.GetData(), CompressedPixels.Num(), &Width, &Height, &ChannelsInFile, 4);
 			if (Pixels)
