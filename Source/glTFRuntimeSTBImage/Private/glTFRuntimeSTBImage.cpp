816716e8e770c2237e0d618a11130de48f214240
rdeioris <roberto.deioris@gmail.com>
2023-07-03 18:20:20 +0200

    updated api

--- Source/glTFRuntimeSTBImage/Private/glTFRuntimeSTBImage.cpp
+++ Source/glTFRuntimeSTBImage/Private/glTFRuntimeSTBImage.cpp
@@ -12,27 +12,56 @@
 
 void FglTFRuntimeSTBImageModule::StartupModule()
 {
+#if defined(GLTFRUNTIME_IMAGE_API_1)
+	FglTFRuntimeParser::OnTexturePixels.AddLambda([](TSharedRef<FglTFRuntimeParser> Parser, TSharedRef<FJsonObject> JsonImageObject, TArray64<uint8>& CompressedPixels, int32& Width, int32& Height, EPixelFormat& PixelFormat, TArray64<uint8>& UncompressedPixels, const FglTFRuntimeImagesConfig& ImagesConfig)
 
+#else
 	FglTFRuntimeParser::OnTexturePixels.AddLambda([](TSharedRef<FglTFRuntimeParser> Parser, TSharedRef<FJsonObject> JsonImageObject, TArray64<uint8>& CompressedPixels, int32& Width, int32& Height, TArray64<uint8>& UncompressedPixels)
+#endif
 		{
 			// skip if already processed
 			if (UncompressedPixels.Num() > 0)
 			{
 				return;
 			}
-			int32 ChannelsInFile;
-			uint8* Pixels = stbi_load_from_memory(CompressedPixels.GetData(), CompressedPixels.Num(), &Width, &Height, &ChannelsInFile, 4);
-			if (Pixels)
+
+	int32 ChannelsInFile;
+#if defined(GLTFRUNTIME_IMAGE_API_1)
+	if (stbi_is_hdr_from_memory(CompressedPixels.GetData(), CompressedPixels.Num()))
+	{
+		PixelFormat = EPixelFormat::PF_FloatRGBA;
+		float* Pixels = stbi_loadf_from_memory(CompressedPixels.GetData(), CompressedPixels.Num(), &Width, &Height, &ChannelsInFile, 4);
+		if (Pixels)
+		{
+			const int64 ImageSize = Width * Height * 4;
+			UncompressedPixels.AddUninitialized(ImageSize * sizeof(uint16));
+			for (int64 Index = 0; Index < ImageSize; Index++)
 			{
-				// RGBA to BGRA
-				const int64 ImageSize = Width * Height * 4;
-				for (int64 IndexR = 0; IndexR < ImageSize; IndexR += 4)
-				{
-					Swap(Pixels[IndexR], Pixels[IndexR + 2]);
-				}
-				UncompressedPixels.Append(Pixels, ImageSize);
-				stbi_image_free(Pixels);
+				FFloat16 Color16(Pixels[Index]);
+				FMemory::Memcpy(UncompressedPixels.GetData() + (Index * sizeof(uint16)), &(Color16.Encoded), sizeof(uint16));
 			}
+			stbi_image_free(Pixels);
+		}
+	}
+	else
+	{
+		PixelFormat = EPixelFormat::PF_B8G8R8A8;
+#endif
+		uint8* Pixels = stbi_load_from_memory(CompressedPixels.GetData(), CompressedPixels.Num(), &Width, &Height, &ChannelsInFile, 4);
+		if (Pixels)
+		{
+			// RGBA to BGRA
+			const int64 ImageSize = Width * Height * 4;
+			for (int64 IndexR = 0; IndexR < ImageSize; IndexR += 4)
+			{
+				Swap(Pixels[IndexR], Pixels[IndexR + 2]);
+			}
+			UncompressedPixels.Append(Pixels, ImageSize);
+			stbi_image_free(Pixels);
+		}
+#if defined(GLTFRUNTIME_IMAGE_API_1)
+	}
+#endif
 		});
 }
 
